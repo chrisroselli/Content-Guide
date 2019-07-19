36eea66cd68c74c28a20e3289ee659e925f7ad7a
WebProduction <WebProduction@admins-iMac-10.local>
2019-07-19 14:45:44 -0400

    Config sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,17 @@
+var getChildren = require('./childscript');
+
 module.exports = {
   title: 'Treehouse Content Guide',
   description: 'Netlify + VuePress',
   themeConfig: {
     docsDir: 'docs',
     repo: 'chrisroselli/content-guide',
-    sidebar: [
-      '/welcome',
-      '/test'
-    ],
+    sidebar: {
+      '/docs/': [{
+        title: 'Docs',
+        children: getChildren('./docs')
+      }]
+    },
     nav: [
       { text: 'Admin', link: '/admin#' }
     ]
